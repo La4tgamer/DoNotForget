0dd22710629a1df988fe1cb614c3af0a281219d2
La4tgamer <32605344+La4tgamer@users.noreply.github.com>
2018-12-14 11:52:41 +0800

    test over

--- DoNotForget/Interface/Form1.cs
+++ DoNotForget/Interface/Form1.cs
@@ -12,7 +12,6 @@ namespace Interface {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-            MessageBox.Show("111");
         }
     }
 }
