d4317bb8a94356838faa94b74e78189afc5e7806
133zmh <564193464@qq.com>
2018-12-20 21:53:04 +0800

    Clock

--- DoNotForget/Interface/Clock.cs
+++ DoNotForget/Interface/Clock.cs
@@ -27,13 +27,13 @@ namespace Interface
             //画小刻度
             for (int i = 0; i < 60; i++)
             {
-                m_graphic.FillRectangle(new SolidBrush(Color.Gray), -2, 2 - m_radius, 1, 5);
+                m_graphic.FillRectangle(new SolidBrush(Color.Gray), -0.5f, 1.5f - m_radius, 1, 5);
                 m_graphic.RotateTransform(6);
             }
             //画大刻度
             for (int i = 0; i < 12; i++)
             {
-                m_graphic.FillRectangle(new SolidBrush(Color.Gray), -3, 2 - m_radius, 2, 10);
+                m_graphic.FillRectangle(new SolidBrush(Color.Gray), -1, 1.5f - m_radius, 2, 10);
                 m_graphic.RotateTransform(30);
             }
             //获取当期时间
@@ -43,30 +43,46 @@ namespace Interface
             //画秒针
             pen.Color = Color.Red;
             m_graphic.RotateTransform(6 * second);
-            m_graphic.DrawLine(pen, 0.5f, 0.5f, 0, (-1) * (float)(m_radius / 1.2));
+            m_graphic.DrawLine(pen, 0, 0, 0, (-1) * (float)(m_radius / 1.2));
             //画分针
             Pen pen1 = new Pen(Color.Blue, 2);
             m_graphic.RotateTransform(-6 * second);
             m_graphic.RotateTransform((float)(0.1 * second + 6 * minute));
-            m_graphic.DrawLine(pen1, 0.5f, 0.5f, 0, (-1) * (float)(m_radius / 1.5));
+            m_graphic.DrawLine(pen1, 0, 0, 0, (-1) * (float)(m_radius / 1.5));
             //画时针
             Pen pen2 = new Pen(Color.Green, 3);
             m_graphic.RotateTransform((float)(0.1 * second + 6 * minute) * (-1));
             m_graphic.RotateTransform((float)(30 / 3600 * second + 30 / 60 * minute + hour * 30));
-            m_graphic.DrawLine(pen2, 0.5f, 0.5f, 0, (-1) * (float)(m_radius / 2.4));
-            Rectangle rect = new Rectangle(-4, -4, 8, 8);
-            m_graphic.FillPie(new SolidBrush(Color.Red), rect, 0, 360);
+            m_graphic.DrawLine(pen2, 0, 0, 0, (-1) * (float)(m_radius / 2.4));
 
             //闹钟
-            /*int minuteC = 9;
-            int hourC =10;
-            Pen pen3_1 = new Pen(Color.Black, 1.8f);
-            Pen pen3_2 = new Pen(Color.Black, 2.8f);
-            m_graphic.RotateTransform((float)(6 * minuteC));
-            m_graphic.DrawLine(pen3_1, 0, 0, 0, (-1) * (float)(m_radius / 1.5));
-            m_graphic.RotateTransform((float)(30 * hourC));
-            m_graphic.DrawLine(pen3_2, 0, 0, 0, (-1) * (float)(m_radius / 2.4));
-            */
+            int minuteC = 9;
+            int hourC = 10;
+            Pen pen3_1 = new Pen(Color.LightBlue, 1.6f);
+            Pen pen4_1 = new Pen(Color.LightBlue, 2.5f);
+            Pen pen3_2 = new Pen(Color.LightCoral, 1.6f);
+            Pen pen4_2 = new Pen(Color.LightCoral, 2.5f);
+            if (hourC < 12)
+            {
+                m_graphic.RotateTransform((float)((30 / 3600 * second + 30 / 60 * minute + hour * 30) * (-1)));
+                m_graphic.RotateTransform((float)(6 * minuteC));
+                m_graphic.DrawLine(pen3_1, 0, 0, 0, (-1) * (float)(m_radius / 1.5));
+                m_graphic.RotateTransform((float)(-6 * minuteC));
+                m_graphic.RotateTransform((float)(30 / 60 * minuteC + 30 * hourC));
+                m_graphic.DrawLine(pen4_1, 0, 0, 0, (-1) * (float)(m_radius / 2.4));
+            }
+            else
+            {
+                m_graphic.RotateTransform((float)((30 / 3600 * second + 30 / 60 * minute + hour * 30) * (-1)));
+                m_graphic.RotateTransform((float)(6 * minuteC));
+                m_graphic.DrawLine(pen3_2, 0, 0, 0, (-1) * (float)(m_radius / 1.5));
+                m_graphic.RotateTransform((float)(-6 * minuteC));
+                m_graphic.RotateTransform((float)(30 / 60 * minuteC + 30 * hourC));
+                m_graphic.DrawLine(pen4_2, 0, 0, 0, (-1) * (float)(m_radius / 2.4));
+            }
+            
+            Rectangle rect = new Rectangle(-4, -4, 8, 8);
+            m_graphic.FillPie(new SolidBrush(Color.Red), rect, 0, 360);
         }
     }
 }
