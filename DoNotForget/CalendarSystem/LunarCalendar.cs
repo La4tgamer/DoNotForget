5ad6c21b36c28472d70f7a67074fd024a1927c6b
HuLinkai <798395174@qq.com>
2018-12-20 18:23:07 +0800

    农历节气

--- DoNotForget/CalendarSystem/LunarCalendar.cs
+++ DoNotForget/CalendarSystem/LunarCalendar.cs
@@ -79,7 +79,7 @@ namespace CalendarSystem
             return string.Concat(GetLunarYear(year), "年", isleap ? "闰" : string.Empty, GetLunarMonth(month), "月", GetLunarDay(day));
         }
 
-        public string terms(DateTime date)
+        public string Terms(DateTime date)
         {
 
             string[] SolarTerm = new string[] { "小寒", "大寒", "立春", "雨水", "惊蛰", "春分", "清明", "谷雨", "立夏", "小满", "芒种", "夏至", "小暑", "大暑", "立秋", "处暑", "白露", "秋分", "寒露", "霜降", "立冬", "小雪", "大雪", "冬至" };
